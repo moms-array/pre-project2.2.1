0e2f5aecb30f559d70e16dd10877707ca17d3912
khaly <khalyavinartem@yandex.ru>
2021-05-05 13:55:17 +0300

    delete variable "query" from listUsers() changed createQuery() in listUsers() and getUsersWithCar()

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -21,15 +21,13 @@ public class UserDaoImp implements UserDao {
    }
 
    @Override
-   @SuppressWarnings("unchecked")
    public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
+      return sessionFactory.getCurrentSession().createQuery("from User",User.class).getResultList();
    }
 
    @Override
    public List<User> getUsersWithCar(String model, int series){
-      TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("select p from User p where p.car.series = :series and p.car.model = :model");
+      TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("select p from User p where p.car.series = :series and p.car.model = :model",User.class);
       query.setParameter("series",series);
       query.setParameter("model",model);
       return query.getResultList();
